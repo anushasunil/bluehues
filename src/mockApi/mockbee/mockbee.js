137796ed40267bdf7bd6504940b84a1ed03d17f5
Anusha <anushasunil71201@gmail.com>
2022-04-10 21:26:32 +0530

    setup mockbee and mockman

---
+++ src/mockApi/mockbee/mockbee.js
@@ -0,0 +1,40 @@
+
+import {Link} from "react-router-dom"
+function MockbeeHome() {
+    return (
+        <header className="App-header">
+        <h1 className="brand-title">
+          Welcome to <span>mockBee!</span>
+        </h1>
+        <p className="brand-description">
+          Get started by editing <code>src/App.js</code>
+        </p>
+        <div className="links">
+          <a
+            href="https://mockbee.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Explore mockBee
+          </a>
+          <a
+            href="https://mockbee.netlify.app/docs/api/introduction"
+            target="_blank"
+            rel="noreferrer"
+          >
+            API Documentation
+          </a>
+          <a
+            href="https://github.com/neogcamp/mockBee"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contribute
+          </a>
+          <Link to="/mockman">Mockman demo</Link>
+        </div>
+      </header>
+    )
+}
+
+export {MockbeeHome} 
