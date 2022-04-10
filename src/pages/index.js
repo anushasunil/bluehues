619e812b032488aef6cd462a6fcd6cbae517823a
Anusha <anushasunil71201@gmail.com>
2022-04-10 21:27:48 +0530

    imported components

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { Home } from "./Home/Home";
+import { MockbeeHome } from "../mockApi/mockbee/mockbee";
+import { MockmanComponent } from "../mockApi/mockman/mockman";
+
+export {Home, MockbeeHome, MockmanComponent}
