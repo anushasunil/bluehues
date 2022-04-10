0add78b0bcdef986c4dec08c24db742ed362319d
Anusha <anushasunil71201@gmail.com>
2022-04-10 21:25:38 +0530

    created `<Footer/>` component and added styles

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.css"
+
+export const Footer = () => {
+    return (
+        <footer>
+            <p className="bold">created by <a className="author-name bold" href="https://github.com/anushasunil">anushasunil</a> - @neogcamp</p>
+        </footer>
+
+    )
+}
