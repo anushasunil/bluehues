bd5127cfa6f5512adda72743e203ae1532e94ce2
Anusha <anushasunil71201@gmail.com>
2022-04-15 19:28:22 +0530

    updated <Nav/> component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import { useLogin } from "../../contexts/login-context"
+import { Link } from "react-router-dom"
 import "./Nav.css"
 
 export const Nav = () => {
@@ -7,17 +8,17 @@ export const Nav = () => {
         <nav className="display-align-center display-justify-space-between">
             <h1 className="brand-name">bluehues</h1>
             <ul className="navbar display-align-center">
-              { !isUserLoggedIn &&  <li className="navbar-action display-align-center clickable-object">
-                    <i className="fa-solid fa-user"></i>
-                    <p>Login</p>
+              { (isUserLoggedIn)? 
+                <li className="navbar-action display-align-center clickable-object">
+                    <i className="fa-solid fa-user navbar-icon"></i>
+                    <p>{userInfo.details.firstName}</p>
+                    <i className="fa-solid fa-power-off navbar-icon" onClick={logoutHandler}></i>
+                </li> 
+                : 
+                <li className="navbar-action display-align-center clickable-object">
+                    <i className="fa-solid fa-user navbar-icon"></i>
+                    <Link to="/login"><p>Login</p></Link>
                 </li>}
-            { isUserLoggedIn &&  <li className="navbar-action display-align-center clickable-object">
-                <i className="fa-solid fa-user navbar-icon"></i>
-                <p>{userInfo.details.firstName}</p>
-            </li>}
-            { isUserLoggedIn && <li className="navbar-action display-align-center clickable-object">
-                <i className="fa-solid fa-power-off navbar-icon" onClick={logoutHandler}></i>
-            </li>}
             </ul>
         </nav>
     )
