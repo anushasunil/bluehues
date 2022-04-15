daa7d3f576915f5242a54764a8c26299696e4ee7
Anusha <anushasunil71201@gmail.com>
2022-04-15 12:51:21 +0530

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Nav, Footer } from "../src/Components"
-import { Home, MockbeeHome, MockmanComponent} from "../src/pages"
+import { Home, MockbeeHome, MockmanComponent, LoginPage, SignUpPage} from "../src/pages"
 
 function App() {
   return (
