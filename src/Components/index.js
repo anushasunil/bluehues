fcad0e8fee1ea1ae9b15913025a33445845cfabb
Anusha <anushasunil71201@gmail.com>
2022-07-01 23:44:31 +0530

    imported all components

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,23 @@
 import { Nav } from "./Nav/Nav";
 import { Footer } from "./Footer/Footer";
+import { Background } from "./Background/Background";
+import { ColorPalette } from "./ColorPalette/ColorPalette";
+import { Aside } from "./Aside/Aside";
+import { NewNote } from "./NewNoteEditor/NewNoteEditor";
+import { TagBox } from "./TagBox/TagBox";
+import { TagSelect } from "./TagBox/TagBox";
+import { clearAllTags } from "./TagBox/TagBox";
+import { NotesToDisplay } from "./NoteCard/NoteCard";
 
-export {Nav, Footer}
+export {
+    Nav, 
+    Footer, 
+    Background, 
+    ColorPalette, 
+    Aside, 
+    NewNote, 
+    NotesToDisplay,
+    TagBox, 
+    TagSelect,
+    clearAllTags
+}
