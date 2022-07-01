989b488163475c66589dcc2b8fa660b69f1f2b1e
Anusha <anushasunil71201@gmail.com>
2022-07-01 23:37:47 +0530

    added route for dashboard page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Nav, Footer } from "../src/Components"
-import { Home, MockbeeHome, MockmanComponent, LoginPage, SignUpPage} from "../src/pages"
+import { Home, MockbeeHome, MockmanComponent, LoginPage, SignUpPage, Dashboard} from "../src/pages"
+import { useLogin } from "./contexts/login/login-context";
 
 function App() {
+  const { isUserLoggedIn } = useLogin();
   return (
     <div className="App">
       <Nav/>
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/dashboard" element={(isUserLoggedIn || localStorage.getItem("token"))? <Dashboard/> : <Navigate replace to="/login"/>}/>
       </Routes>
       <Footer/>
     </div>
