ea17233ed6b6bb16c207a2e22e1ce5b1be78638e
Anusha <anushasunil71201@gmail.com>
2022-07-04 00:07:41 +0530

    fixed code for colorPalette

--- src/Components/ColorPalette/ColorPalette.js
+++ src/Components/ColorPalette/ColorPalette.js
@@ -3,33 +3,23 @@ import { useNotes } from "../../contexts";
 
 export const ColorPalette = ({showPalette}) => {
     const {noteDispatch} = useNotes();
+
+    const colorSet = [
+        "red", "teal", "green", "yellow", "pink"
+    ]
+
     return (
         <div className="color-palette display-align-center" style={{display: showPalette?"flex":"none"}}>
-            <div className="color-pastel red" 
-                onClick={()=>{
-                    noteDispatch({type: "NOTE_COLOR", payload: "var(--color-pastel-red"})
-                }
-            }></div>
-            <div className="color-pastel teal" 
-                onClick={()=>{
-                    noteDispatch({type: "NOTE_COLOR", payload: "var(--color-pastel-teal"})
-                }
-            }></div>
-            <div className="color-pastel green" 
-                onClick={()=>{
-                    noteDispatch({type: "NOTE_COLOR", payload: "var(--color-pastel-green"})
-                }
-            }></div>
-            <div className="color-pastel yellow" 
-                onClick={()=>{
-                    noteDispatch({type: "NOTE_COLOR", payload: "var(--color-pastel-yellow"})
-                }
-            }></div>
-            <div className="color-pastel pink" 
-                onClick={()=>{
-                    noteDispatch({type: "NOTE_COLOR", payload: "var(--color-pastel-pink"})
-                }
-            }></div>
+        {
+            colorSet.map(color => {
+                return (
+                    <div className= {`color-pastel ${color}`} 
+                    onClick={()=>{
+                        noteDispatch({type: "NOTE_COLOR", payload: `var(--color-pastel-${color})` })
+                }}></div>
+                )
+            })
+        }
         </div>
     )
 }
