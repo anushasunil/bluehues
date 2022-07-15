c03a91137e598a7a950618b9953a0b36d1027212
Anusha <anushasunil71201@gmail.com>
2022-07-15 20:17:50 +0530

    added links to other pages and updated css

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,18 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import "./Aside.css"
 
 export const Aside = ({setNewNote}) => {
+    const navigate = useNavigate();
+
     return (
         <aside>
         <ul>
-            <li className="display-align-center clickable-object">
+            <li className="display-align-center clickable-object"
+            onClick={() => {
+                navigate("/dashboard");
+            }}
+            >
                 <i className="fa-solid fa-house inner-icon"></i>
                 <p>Home</p>
             </li>
-            <li className="display-align-center clickable-object">
+            <li className="display-align-center clickable-object"
+            onClick={() => {
+                navigate("/archives");
+            }}
+            >
                 <i className="fa-solid fa-box-archive inner-icon"></i>
                 <p>Archived</p>
             </li>
-            <li className="display-align-center clickable-object">
+            <li className="display-align-center clickable-object"
+            onClick={() => {
+                navigate("/trashedNotes");
+            }}
+            >
                 <i className="fa-solid fa-trash-can inner-icon "></i>
                 <p>Trash</p>
             </li>
@@ -21,7 +36,9 @@ export const Aside = ({setNewNote}) => {
                 <p>Profile</p>
             </li>
         </ul>
-        <button className="solid-primary btn-create-note btn-expanded" onClick={()=>setNewNote("")}>Create a new Note</button>
+        <button className="solid-primary btn-create-note btn-expanded" onClick={()=>setNewNote("")}>
+            Create a new Note
+        </button>
         <button className="solid-primary btn-create-note btn-collapsed" onClick={()=>setNewNote("")}>
             <i className="fa-solid fa-plus"></i>
         </button>
