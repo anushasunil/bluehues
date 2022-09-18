12c0f9a6f2ad161c726600c8d6374f55abc6e486
Anusha <anushasunil71201@gmail.com>
2022-09-18 19:35:54 +0530

    updated `<TagBox/>`
    - discarded feature

--- src/Components/TagBox/TagBox.jsx
+++ src/Components/TagBox/TagBox.jsx
@@ -1,56 +1,52 @@
 import "./TagBox.css";
-import { useState } from "react";
 import { useNotes } from "../../contexts/note/note-context";
+
+const tagsList = ["work", "chores", "creativity", "health"];
 
 const TagBox = () => {
     const {newNote, noteDispatch} = useNotes();
-    const {tags, color} = newNote;
+    const { color, tags } = newNote;
     return (
+        <>
+        <div className="tags-selected-display display-align-center">
+             {
+                 tags.map(tag => {
+                     return(
+                          <div className="tags display-align-center"
+                          style={{backgroundColor: (color === "var(--color-pastel-default)")? "var(--color-tertiary-variant1)" : true}}
+                          >
+                             <p>{tag}</p>
+                             <i className={`fa-solid fa-circle-xmark icon clickable-image ${color}`}
+                             onClick={()=>{
+                                 noteDispatch({type: "TAGS", payload: {action: "REMOVE", value: tag}});
+                             }}
+                             ></i>
+                          </div>
+                     )
+                 })
+             }
+         </div>
         <div className="tags-display display-align-center">
             {
-                tags.map(tag => {
-                    return(
-                         <div className="tags display-align-center">
-                            <p>{tag}</p>
-                            <i className={`fa-solid fa-circle-xmark icon clickable-image ${color}`}
-                            onClick={()=>{
-                                noteDispatch({type: "TAGS", payload: {action: "REMOVE", value: tag}});
-                            }}
-                            ></i>
-                         </div>
+                tagsList.map( tag => {
+                    return (
+                        <div className="tags display-align-center clickable-object"
+                             style={{backgroundColor: (color === "var(--color-pastel-default)")? "var(--color-tertiary-variant1)" : true}}
+                             onClick={()=>{
+                                noteDispatch({type: "TAGS", payload: {action: "ADD", value: tag}})
+                             }}
+                        >
+                           <p>{tag}</p>
+                        </div>
                     )
                 })
             }
         </div>
-    )
-}
-
-const TagSelect = ({showCreateTag, optionDispatch}) => {
-    const [newTag, setNewTag] = useState("");
-    const {newNote, noteDispatch} = useNotes();
-    return (
-        <div className="select-tags display-align-center" style={{display: showCreateTag? "flex": "none"}}>
-            <input 
-                type="text" 
-                className="tag-name" 
-                onChange={(e) => setNewTag(e.target.value)} 
-                autoFocus
-            />
-            <i className="fa-solid fa-plus icons display-align-center clickable-image" 
-                onClick={()=>{
-                    if(!newNote.tags.includes(newTag)) 
-                    noteDispatch({type: "TAGS", payload: {action: "ADD", value: newTag}});
-                    }
-                }>
-            </i>
-            <i className="fa-solid fa-rectangle-xmark icons clickable-image"
-                onClick={()=>optionDispatch({type: "SHOW_TAGS", payload: false})}
-            ></i>
-        </div>
+        </>
     )
 }
 
 export {
-    TagBox, 
-    TagSelect, 
+    tagsList,
+    TagBox
 }
