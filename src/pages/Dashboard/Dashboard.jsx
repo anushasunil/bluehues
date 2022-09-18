a0f1d5f36a381b892d69a015d617bbf9f0dc38e3
Anusha <anushasunil71201@gmail.com>
2022-09-18 19:40:55 +0530

    updated Dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,26 +1,48 @@
 import { useEffect } from "react";
 import { 
     Aside, 
-    NewNote
+    NewNote,
+    DisplayNotes,
+    Filter
 } from "../../Components";
 import { useNotes } from "../../contexts";
 import "./Dashboard.css"
-import "../../Components/NoteCard/NoteCard.css"
 
 export const Dashboard = () => {
-    const {
+    let {
         hideNewNoteEditor, 
         setNewNoteEditor,
-        noteDispatch,
         dataList: {notesList},
         getNotes,
-        archiveNote,
-        deleteNote
+        filtersApplied,
     } = useNotes();
 
     useEffect(()=>{
         getNotes();
-    },[]);
+    }, []);
+
+    const pinnedNotes = notesList.filter(note => note.isPinned === true)
+
+
+    if(filtersApplied.color.length !== 0) {
+        notesList = notesList.filter(note => filtersApplied.color.includes(note.color));
+    }
+    if(filtersApplied.tags.length !== 0) {
+        let temp = [];
+        notesList.map(note =>{
+            let matchCount = 0 ;
+            for(let tag of note.tags) {
+                if(filtersApplied.tags.includes(tag)) {
+                    matchCount++;
+                }
+            }
+            if(matchCount > 0) {
+                temp = [...temp, note]
+            }
+            notesList = [...temp]
+        })
+    }
+
 
     return ( 
         <div className="dashboard">
@@ -30,88 +52,31 @@ export const Dashboard = () => {
                 setNewNoteEditor={setNewNoteEditor}
             />
             <div className="main-container flex-grow">
-            <h4 className="heading">My Notes</h4>
-            {
-                (notesList.length !== 0)?
-                (
-                <div className="notes-container">
-                   { notesList.map(( note ) => {
-                        const {
-                            color, 
-                            title,
-                            tags, 
-                            priority, 
-                            content, 
-                            createdAt
-                        } = note;
+                <Filter/>
+                <h4 className="heading">My Notes</h4>
+                {
+                    (notesList.length !== 0)? (
+                        <>
+                            {
+                                ((pinnedNotes.length !== 0) && (
+                                    <>
+                                        <h5 className="sub-heading">Pinned Notes</h5>
+                                        <DisplayNotes list={pinnedNotes}/>
+                                    </>
+                                )
+                                )
+                            }
+                            <h5 className="sub-heading">All Notes</h5>
+                            <DisplayNotes list={notesList}/>
+                        </>
+                    )   
+                    :
+                    <div className="header-img-empty">
+                        <img src="assets/empty.gif"/>
+                    </div>   
+                }
 
-                        return (
-                    
-                        <div className="note display-flex-column display-justify-space-between" 
-                        style={{backgroundColor: (color == false)? "var(--color-faint-white)" : color}}
-                        key={note._id}
-                        >
-                            <div className="header display-align-center display-justify-space-between">
-                                <h5 className="note-title">
-                                    {title}
-                                </h5>
-                                <p className="badge notification text" 
-                                    style={{display: priority? "block" : "none"}}>
-                                    {priority}
-                                </p>
-                            </div>
-                            <textarea 
-                                value={content} 
-                                spellCheck={false} 
-                                readOnly 
-                                className="note-content"
-                            />
-                            <div className="tags-to-display display-align-center">
-                                {
-                                    tags.map(tagName => {
-                                        return (
-                                            <div className="tags">
-                                                <p>{tagName}</p>
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
-                            <div className="created-details display-align-center">
-                                <small> 
-                                    {createdAt}
-                                </small>
-                            </div>
-                            <div className="action-box display-justify-end">
-                                <i className="fa-solid fa-pen-to-square icons clickable-image"
-                                    onClick={() => {                                            
-                                        noteDispatch({type: "UPDATE_NOTE", payload: note});
-                                        setNewNoteEditor("");
-                                    }}
-                                ></i>
-                                <i className="fa-solid fa-box-archive icons clickable-image"
-                                    onClick={()=>{
-                                        archiveNote(note);
-                                    }}
-                                ></i>
-                                <i className="fa-solid fa-trash-can icons clickable-image"
-                                    onClick={()=>{
-                                        deleteNote(note);
-                                    }}
-                                ></i>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-                </div>)
-                
-        :
-        <div>
-            <img src="assets/empty.gif"/>
-        </div> 
-}
-        </div>
+            </div>
         </div>       
     )
 }        
