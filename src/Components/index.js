efc3f59b60fccfcbc503d6cc8898ef2e8efc70ef
Anusha <anushasunil71201@gmail.com>
2022-09-18 19:35:08 +0530

    updated index.js
    - discarded redundant code

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,19 +1,25 @@
 import { Nav } from "./Nav/Nav";
 import { Footer } from "./Footer/Footer";
 import { ColorPalette } from "./ColorPalette/ColorPalette";
+import { colorSet } from "./ColorPalette/ColorPalette";
 import { Aside } from "./Aside/Aside";
 import { NewNote } from "./NewNoteEditor/NewNoteEditor";
 import { TagBox } from "./TagBox/TagBox";
-import { TagSelect } from "./TagBox/TagBox";
+import { tagsList } from "./TagBox/TagBox";
 import { Toast } from "./Toast/Toast";
+import { DisplayNotes } from "./DisplayNotes/DisplayNotes";
+import { Filter } from "./FilterBox/Filter";
 
 export {
     Nav, 
     Footer, 
-    ColorPalette, 
+    Filter,
+    ColorPalette,
+    colorSet,
+    tagsList, 
     Aside, 
     NewNote, 
     TagBox, 
-    TagSelect,
-    Toast
+    Toast,
+    DisplayNotes
 }
